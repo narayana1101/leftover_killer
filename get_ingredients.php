ed8a0dea72f8de77deec24684ad9a502c07bf1df
LordKomnenos <apalivos@wisc.edu>
2020-10-26 20:29:39 -0500

    version 2.1

--- get_ingredients.php
+++ get_ingredients.php
@@ -1,6 +1,6 @@
 <?php
 
-include('db_config.php');
+include('db_config.php'); // Includes the configuration of database variables
 // Connect to database.
 $con = mysqli_connect($servername, $username, $password, $dbname);
 $response = array();
@@ -11,18 +11,16 @@ if($con) {
     // Now below fetch all the ingredient data.
     $response["ingredients"] = array();
     if($result) {
-        $i = 0; // Loop counter
         // Loop through database table fetching ingrient id,name and image URL.
         while($row = $result->mysqli_fetch_assoc($result) ) {
             // Have an array with index 0,1,2 related to id, name, image respectively
             // into a class Ingredient.
             $Ingredient = array();
-            $Ingredient[$i]['Id'] = $row['ingredient_id'];
-            $Ingredient[$i]['Name'] = $row['ingredient_name'];
-            $Ingredient[$i]['image_url'] = $row['imageURL'];
+            $Ingredient['Id'] = $row['ingredient_id'];
+            $Ingredient['Name'] = $row['ingredient_name'];
+            $Ingredient['image_url'] = $row['imageURL'];
             // Push Ingredient array into list of ingredients List<Ingredient>.
             array_push($response["ingredients"], $Ingredient); 
-            $i++;
         }
     }
 } else {
